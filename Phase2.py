425566f1b46d79d9f9730fcfcf36117ed2ede501
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2017-11-22 09:46:20 -0700

    completed phase2 (untested!)
    title

--- Phase2.py
+++ Phase2.py
@@ -7,6 +7,27 @@ sort -u years.txt
 sort -u recs.txt
 '''
 
-import bsddb3
+#Now make the textfile into files that can easily be read into berkeleyDB
+terms = open("terms.txt", 'r')
+years = open("years.txt", 'r')
+recs = open("recs.txt", 'r')
 
-#?? ??? bsddb3.db_load()
+#New textfiles
+terms_new = open("terms_new.txt", 'w')
+years_new = open("years_new.txt", 'w')
+recs_new = open("recs_new.txt", 'w')
+
+line = "example"
+
+for file_o, file_n in [(terms, terms_new), (years, years_new), (recs, recs_new)]:
+    while(line != "" or line != "\n"): #Keep looping until EOD found
+        line = file_o.readline()
+        if(line == "" or line == "\n"):
+            #End of file has been reached
+            file_o.close()
+            file_n.close()
+            continue
+        
+        #Find colon then split line into two lines
+        line = line.split(':', 1)
+        file_n.write(line[0] + "\n" + line[1] + "\n")
