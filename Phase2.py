ece0cbd83ebf9a49ba839de7e180497df77f8a05
TheBurnerGuy <henryweilo@hotmail.com>
2017-11-21 18:20:16 -0700

    added template phase3 and phase2
    mostly just example code

---
+++ Phase2.py
@@ -0,0 +1,8 @@
+#Phase 2
+#Remember to use these commands after running phase 1
+#Taken from https://www.computerhope.com/unix/usort.htm
+'''
+sort -u terms.txt
+sort -u years.txt
+sort -u recs.txt
+'''
